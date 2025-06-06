1bf70c1afe132219d61b8590e0576a940ce75051
PiyushK-T <piyushtalware333@gmail.com>
2025-06-06 17:40:17 -0400

    minor trial

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/3d-portfolio-website/', 
   plugins: [react()],
   assetsInclude: ['**/*.glb','**/*.gltf'],
-  base: '/3d-portfolio-website',
 })
